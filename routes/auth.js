e9496f5d0ede0b34d645df72352fa7f744d6a028
Simon Chae <s13chae@gmail.com>
2018-07-18 08:33:43 -0700

    seed user database

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET, JWT_EXPIRY } = require('./config');
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 
 const router = express.Router();
 
